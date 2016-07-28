577acfc434980581ca9372a80207287225428f17
Russell Fair <rfair404@gmail.com>
2016-07-28 19:57:13 +0000

    add the plugin base file

---
+++ wp-sld.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+* Plugin Name: Simple Link Directory
+* Author: Russell Fair
+* Description: A minimialist approach to a link directory
+* Version: 0.1
+*/
+load_plugin_textdomain( 'simple-link-directory', false, '/languages/' );
+
+require_once( 'riu-init.php' );
+new ResponsiveImageUpsizer\Init();
